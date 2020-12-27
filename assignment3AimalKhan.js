d6ad765dd84fb11b2d9004ea8a658fd45c0d73c4
Aimal Khan <Aimal.khan@bryanuniversity.edu>
2020-12-26 23:56:12 -0500

    Add files via upload

---
+++ assignment3AimalKhan.js
@@ -0,0 +1,45 @@
+/*
+This code contains only syntax and code style problems. The logic of the code works,
+so DO NOT change the functionality of anything in here.
+
+In short, you shouldn't need to add your own statements anywhere,
+just fix the existing ones.
+*/
+
+
+// Written by Kent, Clark
+
+var Enemies= ["Lex","Batman","Dark side","Brain iac","General Zod","Doomsday"];
+
+function Who_Wins() { ('is-there-kryptonite,enemy name');
+
+if (!is-there-kryptonite);
+{
+    return ("Superman beats " +enemyName+ ", of course");
+
+{
+    return ("Depends on how quick Superman can get rid of the Kryptonite. "+'enemy name' +" could possibly win this one.");
+}
+    }
+
+function HowAttractedIsLoisLaneToMe ()
+{
+    // 1 is not at all attracted, 10 is "super" attracted...
+    return Math.floor((Mat.random()*10)+1);
+}
+
+consol.log( HowAttractedIsLoisLaneToMe ()) ;
+
+var clarkKent = true;
+var superMan = false;
+
+while (clarkKent); 
+    console.log("I'm just a nerdy columnist");
+    var phoneBoothQuickChange =Math.random();
+    if  (phoneBoothQuickChange>= 0.5); 
+        clarkKent=false;
+        superMan=true;
+        console.log("Now I'm Superman!"); 
+    
+    
+}
